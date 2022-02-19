2e564705c14d1e5a2ed5ddc69dfae86fb3b182e7
dhruvjadvani <dhruv.jadvani@gmail.com>
2022-02-19 17:49:23 +0530

    Update BinarySearchMethodLogicOne.java

--- BinarySearchMethodLogicOne.java
+++ BinarySearchMethodLogicOne.java
@@ -6,7 +6,7 @@ public class BinarySearchMethodLogicOne {
 
 	public static void main(String[] args) {
 		
-		int a[] = { 1,2,3,4,5,6,7,8,9,10};
+		int a[] = {1,2,3,4,5,6,7,8,9,10};
 		System.out.println(Arrays.binarySearch(a, 10));
 	}
 
