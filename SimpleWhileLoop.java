09cfe4a4f5e97c66eba954edb46377be5652b1df
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-05 23:19:29 +0530

    Create SimpleWhileLoop.java

---
+++ SimpleWhileLoop.java
@@ -0,0 +1,14 @@
+public class SimpleWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 10) { 
+		   i++; 
+		   if (i % 2 == 0) { 
+		      System.out.print(i + " "); 
+		   } 
+		}
+	}
+
+}
