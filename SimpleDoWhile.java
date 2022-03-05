bb8f2b230dd2b2d8f22e9924eb515991658cab0a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-05 23:20:49 +0530

    Create SimpleDoWhile.java

---
+++ SimpleDoWhile.java
@@ -0,0 +1,13 @@
+public class SimpleDoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		do {
+		    i++; 
+		    System.out.print(i + " "); 
+		    i -= 2; 
+		} while (i > 1);
+	}
+
+}
